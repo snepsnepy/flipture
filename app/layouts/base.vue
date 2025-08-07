0e2a96d3e54fed8259821ddb7940c1d1abf6a9a5
msavulescu <msavulescu@wallbid.io>
2025-08-07 13:55:44 +0300

    Added basic navigation and improved UI.

---
+++ app/layouts/base.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="bg-white min-h-screen">
+    <div class="container mx-auto flex flex-col gap-y-4 lg:gap-y-8">
+      <Navigation />
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,8 +1,12 @@
 <template>
-  <section class="container mx-auto py-10">
+  <section>
     <Hero />
   </section>
 </template>
 <script setup lang="ts">
+definePageMeta({
+  layout: "base",
+});
+
 const name = ref<string>("John Test");
 </script>

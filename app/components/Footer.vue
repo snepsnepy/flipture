616ba31be0e85a734d1ef0f8f6259d0e0cfab4be
msavulescu <msavulescu@wallbid.io>
2025-08-08 20:23:50 +0300

    Added responsiveness

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -9,14 +9,16 @@
       class="relative overflow-hidden"
     >
       <div class="container mx-auto relative z-10">
-        <div class="flex flex-row justify-between items-end">
+        <div
+          class="flex flex-col gap-y-6 lg:flex-row justify-between items-center lg:items-end"
+        >
           <h1
-            class="font-poppins text-center font-bold text-9xl tracking-tighter text-primary-content"
+            class="font-poppins text-center font-bold text-5xl lg:text-9xl tracking-tighter text-primary-content"
           >
             Flipture.
           </h1>
           <span
-            class="font-poppins text-center font-semibold text-primary-content text-xl leading-5"
+            class="font-poppins text-center font-semibold text-primary-content text-base leading-4 lg:text-xl lg:leading-5"
             >© 2025 Flipture. All rights reserved.</span
           >
         </div>
